551e8f718f16c6fdd59bbd59ebad497b948f47f3
Innocent <55434969+InnocentAnyaele@users.noreply.github.com>
2023-02-17 08:44:16 +0000

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ function Header() {
     <div className='flex flex-col items-center pt-5'>
         <img data-testid = 'logo' className='py-4 w-2/3 md:w-1/3 lg:w-1/3' src={developer} alt='developer' />
         <span className='text-2xl pt-1 pb-3 font-bold'>Innocent Anyaele</span>
-        <span className='pb-1 font-semibold'>Software Engineer & Junior Data Professional</span>
+        <span className='pb-1 font-semibold'>Software Engineer</span>
         <span>innocentanyaele2000@gmail.com</span>
         <span>+233 557187667</span>
         <span className='text-blue-600 py-3'><a href='https://github.com/InnocentAnyaele'>Github</a> . <a href='https://www.linkedin.com/in/innocent-anyaele/'>LinkedIn</a></span>
